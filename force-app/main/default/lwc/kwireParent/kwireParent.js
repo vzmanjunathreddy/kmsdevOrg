9a31380961aa30f4cb0837257d914958a1a6ebdb
vzmanjunathreddy <vz.manjunathreddy@gmail.com>
2023-04-03 20:55:49 +0530

    Fixed the issue

--- force-app/main/default/lwc/kwireParent/kwireParent.js
+++ force-app/main/default/lwc/kwireParent/kwireParent.js
@@ -1,8 +1,26 @@
-import { LightningElement,wire } from 'lwc';
-import getHotelsData from  '@salesforce/apex/KHotelController.getHotelDetails';
+import { LightningElement,track,wire } from 'lwc';
+import getHotelsData from  '@salesforce/apex/KHotelController.getHotelsData';
 
 export default class KwireParent extends LightningElement {
 
+    @track
+    hotelsdata;
+
+    @track
+    errordata;
+
     @wire(getHotelsData)
-    hotels;
+    hotels({error, data})
+    {
+        if(data)
+        {
+            console.log(data);
+            this.hotelsdata=data;
+        }
+        else
+        {
+            console.log(data);
+              this.errordata=error;  
+        }
+    };
 }
